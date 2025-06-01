4600859dfc248c12a47c1bc2d07e445bbdd60672
sleman <slemandb21@gmail.com>
2025-06-01 19:58:49 +0300

    complete two api updatuser(role) and suspend users

--- server/src/controllers/Dashboard_managment/Dashboard.controller.js
+++ server/src/controllers/Dashboard_managment/Dashboard.controller.js
@@ -21,3 +21,49 @@ module.exports.updatePermission = async (req, res) => {
       res.status(400).json({ error: err.message });
   }
 }
+
+module.exports.updateUser =  async (req, res) => {
+  try {
+      const { id } = req.params;
+      const updates = req.body;
+      const user = await Users.findByIdAndUpdate(id, updates, { new: true, runValidators: true });
+
+      if (!user) {
+          return res.status(404).json({ error: 'User not found' });
+      }
+
+      res.status(200).json({status:true,message:'successfuly..!',data:null});
+  } catch (err) {
+      res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports.suspendedUser = async (req, res, next) => {
+    const user_id = req.params.userId;
+    
+    const { duration } = req.body;  
+    try {
+        const user = await Users.findById(user_id,"username isSuspended suspendedUntil ");
+
+        if (!user) {
+            const errors = ['User not found'];
+            const error = appError.create(errors[0], 404, false, errors);
+            return next(error);
+        }
+
+        //Calculate the date and time of the suspension end
+        const suspendedUntil = new Date();
+        suspendedUntil.setMinutes(suspendedUntil.getMinutes() + duration);
+
+        user.isSuspended = true;
+        user.suspendedUntil = suspendedUntil;
+
+        await user.save();
+
+        res.status(200).json({ status: true, message: "User suspended successfully", data: { user } });
+    } catch (err) {
+        const errors = [err.message || err];
+        const error = appError.create(errors[0], 500, false, errors);
+        return next(error);
+    }
+};
--- server/src/routes/Dashboard/Dashboard.routes.js
+++ server/src/routes/Dashboard/Dashboard.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = express.Router();
-const employeeMangmentController = require('../../controllers/Employee_mangment.js/employee.controller')
+const dashboard_usermangment_controller = require('../../controllers/Dashboard_managment/Dashboard.controller')
 const authorize = require('../../middelWare/allowedToJs')
 const verifyToken = require('../../middelWare/verifyToken')
 
 routes.route("/users/:id/permissions").put(verifyToken,authorize('update', 'permissions'),dashboard_usermangment_controller.updatePermission)
+routes.route('/users/:id').put(verifyToken,authorize('update', 'role'),dashboard_usermangment_controller.updateUser)
+routes.route("/suspendedUser/:userId").post(verifyToken,authorize('update', 'suspendUsers'),dashboard_usermangment_controller.suspendedUser)
 
 
 
